f212e84d46db58f479812f621195e6c1d6c724ac
Barnaby Cotter <barnabycotter@gmail.com>
2020-05-21 13:29:54 +0100

    Re add accidental prettier removal

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,8 @@ module.exports = {
     'eslint:recommended',
     "plugin:react/recommended",
     'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
   ],
   env: {
       'browser': true,
