b3e2598426ad42dc3674cfdd52ecadb1367c2333
Daniel Burnley <dan@danops.uk>
2020-06-03 15:53:23 +0100

    Add load balancing to model deployments

--- model/terraform/main.tf
+++ model/terraform/main.tf
@@ -14,7 +14,7 @@ terraform {
 }
 
 resource "aws_vpc" "antibody-model" {
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.0.0/16"
 
   tags = {
     Name = "antibody-model"
@@ -52,9 +52,20 @@ resource "aws_main_route_table_association" "antibody-model-main-route-table" {
 }
 
 
-resource "aws_subnet" "antibody-model" {
-  vpc_id     = "${aws_vpc.antibody-model.id}"
-  cidr_block = "10.0.1.0/24"
+resource "aws_subnet" "antibody-model-1" {
+  vpc_id               = "${aws_vpc.antibody-model.id}"
+  cidr_block           = "10.0.0.0/24"
+  availability_zone_id = "euw2-az1"
+
+  tags = {
+    Name = "antibody-model"
+  }
+}
+
+resource "aws_subnet" "antibody-model-2" {
+  vpc_id               = "${aws_vpc.antibody-model.id}"
+  cidr_block           = "10.0.1.0/24"
+  availability_zone_id = "euw2-az2"
 
   tags = {
     Name = "antibody-model"
@@ -65,16 +76,31 @@ resource "aws_default_security_group" "antibody-model-default-security-group" {
   vpc_id = "${aws_vpc.antibody-model.id}"
 
   ingress {
-    protocol    = "tcp"
-    from_port   = 8080
-    to_port     = 8080
+    protocol        = "tcp"
+    from_port       = 8080
+    to_port         = 8080
+    security_groups = ["${aws_security_group.lb-security-group.id}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  tags = {
+    Name = "antibody-model"
+  }
+}
+
+resource "aws_security_group" "lb-security-group" {
+  vpc_id = "${aws_vpc.antibody-model.id}"
+
   ingress {
     protocol    = "tcp"
-    from_port   = 8081
-    to_port     = 8081
+    from_port   = 80
+    to_port     = 80
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -87,6 +113,39 @@ resource "aws_default_security_group" "antibody-model-default-security-group" {
 
   tags = {
     Name = "antibody-model"
+  }
+}
+
+resource "aws_lb" "antibody-model" {
+  name               = "antibody-model-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = ["${aws_security_group.lb-security-group.id}"]
+  subnets            = ["${aws_subnet.antibody-model-1.id}", "${aws_subnet.antibody-model-2.id}"]
+}
+
+resource "aws_lb_target_group" "antibody-model" {
+  name        = "antibody-model-target-group"
+  vpc_id      = "${aws_vpc.antibody-model.id}"
+  port        = 8080
+  protocol    = "HTTP"
+  target_type = "ip"
+
+  health_check {
+    enabled = true
+    interval = 10
+    path = "/ping"
+  }
+}
+
+resource "aws_lb_listener" "antibody-model" {
+  load_balancer_arn = "${aws_lb.antibody-model.arn}"
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.antibody-model.arn}"
   }
 }
 
@@ -109,9 +168,21 @@ resource "aws_ecs_service" "antibody-model-service" {
   cluster         = "${aws_ecs_cluster.antibody-model-cluster.id}"
   task_definition = "${aws_ecs_task_definition.antibody-model-task-definition.arn}"
   launch_type     = "FARGATE"
+
   network_configuration {
     assign_public_ip = true
-    subnets          = ["${aws_subnet.antibody-model.id}"]
+    subnets          = ["${aws_subnet.antibody-model-1.id}", "${aws_subnet.antibody-model-2.id}"]
   }
+
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.antibody-model.arn}"
+    container_name   = "antibody-model-container"
+    container_port   = 8080
+  }
+
   desired_count = 1
+
+  depends_on = [
+    "aws_lb_listener.antibody-model"
+  ]
 }
