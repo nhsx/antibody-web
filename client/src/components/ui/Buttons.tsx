3ba13970b1483090cfe093ebd03dfc872848c392
Daniel Burnley <dan@danops.uk>
2020-06-04 16:19:47 +0100

    Update next button to use link

--- client/src/components/ui/Buttons.tsx
+++ client/src/components/ui/Buttons.tsx
@@ -3,7 +3,7 @@
 // Use of this source code is governed by an LGPL-3.0 license that
 // can be found in the LICENSE file distributed with this file.
 import React, { FunctionComponent } from 'react';
-import { ActionLink } from 'nhsuk-react-components';
+import { Link } from 'react-router-dom'
 
 export interface ContinueButtonProps {
   text?: string;
@@ -14,6 +14,10 @@ export const ContinueButton: FunctionComponent<ContinueButtonProps> = (
   props: ContinueButtonProps
 ) => {
   return (
-    <ActionLink href={props.href ? props.href : ""} >{props.text ? props.text : 'Next'}</ActionLink>
+    <Link
+      className="nhsuk-button"
+      to={props.href ? props.href : ""}>
+      {props.text ? props.text : 'Next'}
+    </Link>
   );
 };
