4e15691e0df207d189b3ed2424545277a77c8bb2
Daniel Burnley <dan@danops.uk>
2020-06-18 15:53:24 +0100

    Test the default export of TestRoutes rather than individual bits

--- client/src/routes/__tests__/TestRoutes.test.tsx
+++ client/src/routes/__tests__/TestRoutes.test.tsx
@@ -1,71 +1,68 @@
 import React from 'react';
 import { MemoryRouter } from "react-router-dom";
 import { screen } from "@testing-library/react";
-import { TestRoutes, PreviewRoutes } from "routes/TestRoutes";
+import TestRoutes from "routes/TestRoutes";
 import { renderWithReactIntl } from "utils/testUtils";
 import AppContext from 'components/App/context';
-import TestContext from 'components/TestContainer/context';
 
 jest.mock("utils/AppConfig", () => ({
   getAppConfig: () => ({ imageUpload: false })
 }));
 
 describe("TestRoutes", () => {
-  describe("Support steps", () => {
-    beforeEach(() => {
-      renderWithReactIntl(
-        <MemoryRouter initialEntries={["/test/checkYourKit"]}>
+  const renderTestStep = ({ mode, step }: { mode: string, step: string }) => {
+    const appContext = {
+      state: { locale: "en-gb" },
+      setLocale: () => { },
+      setAppError: () => { },
+      dispatch: () => { },
+      container: {
+        getTestApi: () => ({
+          generateTest: () => ({ testRecord: { timerStartedAt: 10 } })
+        })
+      }
+    };
+
+    renderWithReactIntl(
+      <AppContext.Provider value={appContext} >
+        <MemoryRouter initialEntries={[`/${mode}/${step}`]}>
           <TestRoutes />
         </MemoryRouter>
-      );
-    });
+      </AppContext.Provider >
+    );
+  };
 
+
+  describe("Support steps", () => {
     it("Renders the content component", async () => {
+      renderTestStep({ mode: "test", step: "checkYourKit" });
       const content = await screen.findByText(/Check Your Kit/);
       expect(content).not.toBeUndefined();
     });
 
     it("There is a begin test link ", async () => {
+      renderTestStep({ mode: "test", step: "checkYourKit" });
       const content = await screen.findByText(/Begin/);
       expect(content).not.toBeUndefined();
     });
   });
 
   describe("Test steps", () => {
-    const renderTestStep = ({ step }: { step: string }) => {
-      renderWithReactIntl(
-        <AppContext.Provider value={{ state: { locale: "en-gb" }, setLocale: () => { }, setAppError: () => { }, dispatch: () => { }, container: { getTestApi: () => { } } }} >
-          <TestContext.Provider
-            value={{
-              dispatch: () => { }, state: {
-                testRecord: {
-                  uploadUrl: "", downloadUrl: "", step, guid: "", timerStartedAt: 10
-                }
-              }
-            }}>
-            <MemoryRouter initialEntries={[`/test/${step}`]}>
-              <TestRoutes />
-            </MemoryRouter>
-          </TestContext.Provider>
-        </AppContext.Provider >
-      );
-    };
-
     it("Renders the content component", async () => {
-      renderTestStep({ step: "washAndDryHands" });
+      renderTestStep({ mode: "test", step: "washAndDryHands" });
       const content = await screen.findByText(/Wash and dry hands/);
       expect(content).not.toBeUndefined();
     });
 
     it("Renders the correct number of steps", async () => {
-      renderTestStep({ step: "washAndDryHands" });
+      renderTestStep({ mode: "test", step: "washAndDryHands" });
       const content = await screen.findByText(/Step 1 of 11/);
       expect(content).not.toBeUndefined();
     });
 
     describe("With no image upload enabled", () => {
       it("Skips from the wait step to the what do you see step", async () => {
-        renderTestStep({ step: "wait" });
+        renderTestStep({ mode: "test", step: "wait" });
         const content = await screen.findByTestId("primary-button");
         expect(content.getAttribute('href')).toBe("/test/whatDoYouSee");
       });
@@ -73,28 +70,20 @@ describe("TestRoutes", () => {
   });
 
   describe("Previewing steps", () => {
-    const renderPreviewStep = ({ step }: { step: string }) => {
-      renderWithReactIntl(
-        <MemoryRouter initialEntries={[`/preview/${step}`]}>
-          <PreviewRoutes />
-        </MemoryRouter>
-      );
-    };
-
     it("Allows you to preview content", async () => {
-      renderPreviewStep({ step: "washAndDryHands" });
+      renderTestStep({ mode: "preview", step: "washAndDryHands" });
       const content = await screen.findByText(/Wash and dry hands/);
       expect(content).not.toBeUndefined();
     });
 
     it("Renders the correct number of steps", async () => {
-      renderPreviewStep({ step: "washAndDryHands" });
+      renderTestStep({ mode: "preview", step: "washAndDryHands" });
       const content = await screen.findByText(/Step 1 of 8/);
       expect(content).not.toBeUndefined();
     });
 
     it("Links back to the initial test step on the final previewable step", async () => {
-      renderPreviewStep({ step: "testBloodSample" });
+      renderTestStep({ mode: "preview", step: "testBloodSample" });
       const content = await screen.findByTestId("primary-button");
       expect(content.getAttribute('href')).toBe("/test");
     });
