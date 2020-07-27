932e1a968e6d20c40d784a56d722c02693b885e5
Daniel Burnley <dan@danops.uk>
2020-07-27 13:54:24 +0100

    [LDAT-146] Add tests for testApi.update

---
+++ client/src/api/__tests__/testApi.test.ts
@@ -0,0 +1,83 @@
+import testApi, { HTTPError } from "../testApi";
+import nock from "nock";
+
+jest.mock("js-cookie", () => ({
+  get: () => "stubCookie",
+}));
+
+describe("TestApi", () => {
+  const apiBase = "https://meow.cat/dev";
+
+  const testErrors = ({ stubRequest, callApi, expectedErrorCode }) => {
+    describe("returning errors", () => {
+      it("Throws an error", async () => {
+        stubRequest();
+        await expect(callApi()).rejects.toThrow(HTTPError);
+      });
+
+      it("Returns the status code in the error", async () => {
+        stubRequest();
+        let error: HTTPError | undefined;
+
+        try {
+          await callApi();
+        } catch (e) {
+          error = e;
+        }
+
+        expect(error).not.toBeUndefined();
+        expect(error?.statusCode).toBe(expectedErrorCode);
+      });
+    });
+  };
+
+  beforeEach(() => {
+    nock(apiBase).options(/.*/).reply(
+      200,
+      {},
+      {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Authorization, Content-Type",
+      }
+    );
+  });
+
+  describe("/update", () => {
+    it("Passes the parameters & cookie to the update endpoint", async () => {
+      const updateRequest = nock("https://meow.cat/dev")
+        .post("/update", { dog: "woof" })
+        .matchHeader("Authorization", "stubCookie")
+        .reply(200, {});
+
+      await testApi({ apiBase: "https://meow.cat/dev" }).updateTest({
+        dog: "woof",
+      });
+
+      expect(updateRequest.isDone()).toBe(true);
+    });
+
+    it("Returns the response from the update endpoint", async () => {
+      nock("https://meow.cat/dev")
+        .post("/update", { dog: "woof" })
+        .reply(200, { an: "example" });
+
+      const response = await testApi({
+        apiBase: "https://meow.cat/dev",
+      }).updateTest({ dog: "woof" });
+
+      expect(response).toEqual({ an: "example" });
+    });
+
+    testErrors({
+      stubRequest: () =>
+        nock("https://meow.cat/dev")
+          .post("/update", { dog: "woof" })
+          .reply(403),
+      callApi: () =>
+        testApi({ apiBase: "https://meow.cat/dev" }).updateTest({
+          dog: "woof",
+        }),
+      expectedErrorCode: 403,
+    });
+  });
+});
