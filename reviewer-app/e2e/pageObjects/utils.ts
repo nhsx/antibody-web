50f68a8971f38223934abd7c663f1e00d656fb54
Barnaby Cotter <barnabycotter@gmail.com>
2020-08-10 17:15:46 +0100

    unshare base ts file for now, fix some issues with renaming

--- reviewer-app/e2e/pageObjects/utils.ts
+++
@@ -1,8 +0,0 @@
-import { root } from './index';
-
-export const testId = (id) => `[data-testid="${id}"]`;
-
-export const testElem = async (id) => {
-  const app = await root();
-  return await app.$(testId(id));
-};
