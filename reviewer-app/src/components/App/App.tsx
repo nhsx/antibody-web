bc626bf549cfcbd490851b383c252384945da088
Daniel Burnley <dan@danops.uk>
2020-08-17 11:22:20 +0100

    [LDAT-229] Add NHS design system grid

--- reviewer-app/src/components/App/App.tsx
+++ reviewer-app/src/components/App/App.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import TestResult from '../TestResult';
 import appContainer from './container';
-import './App.css';
+import { Header, Container, Row, Col } from 'nhsuk-react-components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <TestResult container={appContainer} />
-      </header>
-    </div>
+    <>
+      <Header transactional>
+        <Header.Container>
+          <Header.Logo href="/" />
+          <Header.ServiceName href="/">
+            Coronavirus home testing
+          </Header.ServiceName>
+        </Header.Container>
+      </Header>
+      <Container>
+        <main className="nhsuk-main-wrapper nhsuk-main-wrapper--s">
+          <Row>
+            <Col width="two-thirds">
+              <TestResult container={appContainer} />
+            </Col>
+          </Row>
+        </main>
+      </Container>
+    </>
   );
 }
 
