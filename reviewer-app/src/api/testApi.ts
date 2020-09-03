db4d921d2a248cb519bd1c1cb53661197f5b951e
Barnaby Cotter <barnabycotter@gmail.com>
2020-09-03 08:44:28 +0100

    Handle 204 response

--- reviewer-app/src/api/testApi.ts
+++ reviewer-app/src/api/testApi.ts
@@ -17,6 +17,7 @@ export class HTTPError extends Error {
 
 // We throw on HTTP failures
 function handleErrors(response: Response): Response {
+  console.log(response);
   if (!response.ok) {
     throw new HTTPError({
       message: response.statusText,
@@ -35,6 +36,10 @@ export default ({ apiBase }: { apiBase: string }): TestApi => ({
       },
     }).then(handleErrors);
 
-    return response.json();
+    if (response.status === 204) {
+      return null;
+    } else {
+      return response.json();
+    }
   },
 });
